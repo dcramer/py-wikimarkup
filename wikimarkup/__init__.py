0396a517fbf825d0dd81ca29e586d71634d76504
David Cramer <dcramer@gmail.com>
2008-07-31 08:38:26 +0000

    added email

--- wikimarkup/__init__.py
+++ wikimarkup/__init__.py
@@ -2,7 +2,7 @@
 """
 MediaWiki-style markup
 
-Copyright (C) 2008 David Cramer
+Copyright (C) 2008 David Cramer <dcramer@gmail.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
