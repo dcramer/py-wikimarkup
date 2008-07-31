4b786070392fcbc9122411b23173a458a230f8a5
David Cramer <dcramer@gmail.com>
2008-07-31 08:31:32 +0000

    initial commit + no-django patch

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='wikimarkup',
+      version='1.01',
+      description='Wiki Markup (MediaWiki)',
+      author='David Cramer',
+      author_email='dcramer@gmail.com',
+      url='http://code.google.com/p/wikimarkup/',
+      packages=['wikimarkup'],
+     )
