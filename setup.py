0dc23f90f1988e03096a83f7040b56f0c56a497e
David Cramer <dcramer@gmail.com>
2008-08-22 20:09:56 +0000

    updated distutils

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@
 
 from distutils.core import setup
 
-setup(name='wikimarkup',
-      version='1.01',
-      description='Wiki Markup (MediaWiki)',
-      author='David Cramer',
-      author_email='dcramer@gmail.com',
-      url='http://code.google.com/p/wikimarkup/',
-      packages=['wikimarkup'],
-     )
+setup(
+    name='wikimarkup',
+    version='1.01',
+    description='A basic MediaWiki markup parser.',
+    author='David Cramer',
+    author_email='dcramer@gmail.com',
+    url='http://code.google.com/p/wikimarkup/',
+    packages=['wikimarkup'],
+)
